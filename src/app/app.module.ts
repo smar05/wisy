ca0896dec6714f3f39913d4c5bb45a8fd21aaac6
Ricardo Mantilla <mantillasanchezr@gmail.com>
2024-07-14 14:34:16 -0500

    Navbar and bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PagesRoutingModule } from './pages/pages-routing.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavbarComponent],
   imports: [BrowserModule, AppRoutingModule, PagesRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
