17d022874ac300e624bd59e634ae3f1ca61bac7f
Ricardo Mantilla <mantillasanchezr@gmail.com>
2024-07-15 12:53:35 -0500

    Validate the weather option

--- src/app/pages/weather/weather.component.ts
+++ src/app/pages/weather/weather.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { EnumRoutes } from 'src/app/enums/enums-routes';
+import { EnumWeatherOptions } from 'src/app/enums/enums-weather-options';
 
 @Component({
   selector: 'app-weather',
@@ -13,10 +14,24 @@ export class WeatherComponent implements OnInit {
   constructor(private route: ActivatedRoute, private router: Router) {}
 
   public ngOnInit(): void {
+    this.getUrlParam();
+  }
+
+  /**
+   * Get the param from URL
+   *
+   * @private
+   * @memberof WeatherComponent
+   */
+  private getUrlParam(): void {
     this.route.paramMap.subscribe((params) => {
       this.weatherOption = params.get('weatherOption') as string;
       if (this.weatherOption !== null) {
-        this.weatherOption = this.weatherOption.toString();
+        // Validate that url param belowns to a weather option
+        let weatherOptions: string[] = Object.keys(EnumWeatherOptions);
+
+        if (!weatherOptions.includes(this.weatherOption))
+          this.router.navigate([`/${EnumRoutes.HOME}`]);
       } else {
         this.router.navigate([`/${EnumRoutes.HOME}`]);
       }
